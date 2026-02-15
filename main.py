b0818e8672e1129b11ed850734754a038ce59156
sarthakworks <sarthak.tiet@gmail.com>
2026-02-15 11:03:15 +0530

    get names

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ app = FastAPI()
 def root():
     return {"message": "Hello from FastAPI!"}
 
+
+# Define a GET API endpoint "/greet" with Query parameters
+# In Browser, access the endpoint like this:
+# "/greet?first_name=sarthak&last_name=bansal"
+@app.get("/greet")
+def greet(first_name: str, last_name: str):
+    return {"message": f"Hello {first_name} {last_name}!"}
+
+
+# Define a GET API endpoint "/hello" with Path parameters
+# In Browser, access the endpoint like this: "/hello/Amar/Kumar"
+@app.get("/hello/{fname}/{lname}")
+def get_item(fname: str, lname: str):
+    msg = "Hello! " + fname + " " + lname
+    return {"message": msg}
+
 # Start application
 if __name__ == "__main__":
     uvicorn.run(app)
